72bed7394264cdc8d81b6a10d9720012b9852f10
name surname <full@email.com>
2023-03-08 14:14:37 +0200

    lesson_17

---
+++ lesson_17/exersice/exercise_1.py
@@ -0,0 +1,17 @@
+# Create a Calculator class with main functionality: add, divide, multiply, subtract ,
+# etc.. Initiate class and show up some calculations.
+
+# from typing import Union
+
+# class Calculator:
+
+# def _init_(self, number_one: int, number_two: int):
+
+#     try:
+#         sum = number_one + number_two
+#         substraction = number_one - number_two
+#         division = number_one / number_two
+#         multiplication = number_one * number_two
+#         return f"sum_s = {sum}, substraction = {substraction}, division = {division}, multiplication = {multiplication}"
+
+# print(calculator(10, 0))
---
+++ lesson_17/main.py
@@ -0,0 +1,8 @@
+class Person:
+    def _init_(self, name: str, surname: str):
+        self.name = name
+        self.surname = surname
+person = Person ("Remis")
+print(person)
+
+
